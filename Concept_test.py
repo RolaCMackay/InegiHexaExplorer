c1e975611345932f1b36634ae93c0bfafbc3df1e
Rolando <rolando.chavez@enviaflores.com>
2024-01-09 15:07:13 -0600

    data

---
+++ Concept_test.py
@@ -0,0 +1,14 @@
+# Concept test
+
+
+# TODO tesselate a surface using hexagons
+
+# TODO asign arbitrary values to each hexagon
+
+# TODO Create another surface tesselated with a hexagen where each side is 3 times the a value of the last hexagons
+
+# TODO map values from the first surface to the second surface
+
+# TODO Automate this to N layers
+
+# 
